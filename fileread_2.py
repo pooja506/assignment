450bbf0eb3cac07331bebcdf8bbf5af3c1f2b0cc
Sabeen Shrestha <70570881+sabeensstha@users.noreply.github.com>
2022-08-27 11:02:56 +0545

    comment added to q No 2

--- fileread_2.py
+++ fileread_2.py
@@ -1,8 +1,8 @@
 headers = ["Name", "Age", "City"]
 
-print("\t \t".join(headers))
-print("+-------------+----------------+-------------- ")
-for line in open("person.txt", "r"):
+print("\t \t".join(headers)) # header added to format with tab
+print("+-------------+----------------+-------------- ") # design format
+for line in open("person.txt", "r"): # using loop to read data from each line from text file person.txt
     line = line.strip().split("  ")
-    print("\t \t".join(line))
+    print("\t \t".join(line)) # displaying data in formatted form
     print("\n")
